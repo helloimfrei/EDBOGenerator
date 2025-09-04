cbef6dab42f3b18acdf2b5b9f6465e24c6ecf40a
helloimfrei <christianjtapp@gmail.com>
2025-09-04 10:55:20 -0400

    fixing versions

--- edbogenerator/edbogenerator/__init__.py
+++ edbogenerator/edbogenerator/__init__.py
@@ -1,1 +1,6 @@
 from .generator import EDBOGenerator
+
+import sys
+if sys.version_info >= (3, 10):
+    raise RuntimeError("Python 3.10+ not supported with EDBO 0.2.0 / torch 1.10. "
+                       "Use Python 3.8–3.9.")
